2c458f508aef25e6f8be55c515a5bb7a0017d9da
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2019-11-17 09:08:14 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ sudo pacman -S xorg-server xorg xorg-xinit
 sudo pacman -S moka-icon-theme
 sudo pacman -S w3m jwm conky
 sudo pacman -S pavucontrol pulseaudio
-sudo pacman -S gmrun termite
+sudo pacman -S gmrun termite ntfs-3g
 sudo pacman -S neofetch rofi oblogout gtk2-perl lsb-release
 sudo pacman -S adapta-gtk-theme gvfs pdfmixtool xpdf netsurf
 sudo pacman -S virtualbox-guest-utils
